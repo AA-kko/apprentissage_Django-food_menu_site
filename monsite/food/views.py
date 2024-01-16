d92630ac95278c35373081cad5733150ac846208
kone <kkoonneekkeeiittaa@gmail.com>
2024-01-16 20:44:33 +0100

    ajout de du fichier index.html pour  avoir le template affichant la page de démarrage

--- monsite/food/views.py
+++ monsite/food/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('hello world')
+    item_list = Item.objects.all()
+    template = loader.get_template('food/index.html')
+    context = {
+
+
+    }
+    return HttpResponse(template.render(context,request))
 
 def item(request):
     return HttpResponse('This is an item view')
