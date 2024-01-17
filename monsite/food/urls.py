5851ecc842b97ae92fd08efb914f2c4aa94c11ce
kone <kkoonneekkeeiittaa@gmail.com>
2024-01-17 20:50:48 +0100

    test obsolete

--- monsite/food/urls.py
+++ monsite/food/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #avant il y a toujours /food/
     path('',views.index,name='index'),
     path('item/',views.item,name='item'),
+    path('<int:item_id>',views.detail,name='detail'),
  ]
